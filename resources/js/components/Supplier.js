8d524ccb26a9fa96a55424861b1021f51405a3d9
ssapalo <stephenjohnsapalo@gmail.com>
2021-03-22 23:57:21 +0800

    changes

--- resources/js/components/Supplier.js
+++ resources/js/components/Supplier.js
@@ -1,20 +1,108 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import {Button,Container,Row,Table,Modal,ModalTitle,ModalDialog,ModalBody,ModalDialogProps,ModalFooter} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusSquare,faTrashAlt,faEye} from '@fortawesome/free-solid-svg-icons';
+
+function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+}
 
 function Supplier() {
-    
+    const buttonStyle ={
+        display:"flex",
+        justifyContent:"flex-end",
+        marginBottom:"1rem"
+    };
     return (
         
         <div>
-            <Breadcrumb>
-                <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-                <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
-                    Library
-                </Breadcrumb.Item>
-                <Breadcrumb.Item active>Data</Breadcrumb.Item>
-            </Breadcrumb>
+            <Container fluid>
+                <div style={buttonStyle}>
+                    <Button variant="success" size="sm" style={{marginRight:"0.5rem"}}>
+                        <FontAwesomeIcon icon={faPlusSquare}/>
+                        Add Supplier   
+                    </Button>
+                </div>
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                        <th>Supplier ID</th>
+                        <th>Supplier</th>
+                        <th>Contact Number</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>1</td>
+                        <td>Jollibee</td>
+                        <td>69</td>
+                        <td>jabee@manamit.com</td>
+                        <td> 
+                            <Button variant="outline-info" size="sm">
+                            <FontAwesomeIcon icon={faEye}/>View</Button>
+                            <Button variant="outline-danger" size="sm">
+                                <FontAwesomeIcon icon={faTrashAlt}/>Delete
+                            </Button>
+                        </td>
+                        </tr>
+                        <tr>
+                        <td>2</td>
+                        <td>Mcdonalds</td>
+                        <td>163</td>
+                        <td>jcdo@jolibee.com</td>
+                        <td> 
+                            <Button variant="outline-info" size="sm">
+                            <FontAwesomeIcon icon={faEye}/>View</Button>
+                            <Button variant="outline-danger" size="sm">
+                                <FontAwesomeIcon icon={faTrashAlt}/>Delete
+                            </Button>
+                        </td>
+                        </tr>
+                        <tr>
+                        <td>3</td>
+                        <td>Grab</td>
+                        <td>123</td>
+                        <td>Grab@gmail.com</td>
+                        <td> 
+                            <Button variant="outline-info" size="sm">
+                            <FontAwesomeIcon icon={faEye}/>View</Button>
+                            <Button variant="outline-danger" size="sm">
+                                <FontAwesomeIcon icon={faTrashAlt}/>Delete
+                            </Button>
+                        </td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Container>
         </div>
+       
     );
 }
 
