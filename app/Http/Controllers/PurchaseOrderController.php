4616eaecaa4a582e76df1d3e99380044eaadacf3
Rose Canoy <rose369canoy@gmail.com>
2021-05-20 01:04:15 +0800

    middleware

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -7,6 +7,11 @@ use App\Helpers\PurchaseOrderHelper;
 
 class PurchaseOrderController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
