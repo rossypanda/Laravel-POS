234e3c02d9259c11d7dd7b5974cc475ef07697bd
Rose Canoy <rose369canoy@gmail.com>
2021-06-19 02:48:44 +0800

    poReportLaravel lng danay

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        \Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -228,7 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
-
+        'DataTables' => \Yajra\DataTables\Facades\DataTables::class,
     ],
 
 ];
